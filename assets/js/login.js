b0839aaf8bb9d1bed095ec4d954a80ca441f3978
mengqingge <1079125857@qq.com>
2020-06-27 19:50:03 +0800

    完成了注册和登录的功能3

--- assets/js/login.js
+++ assets/js/login.js
@@ -57,7 +57,7 @@ $(function () {
             data: $(this).serialize(),
             success: res => {
                
-                if (res.status !== 0) {
+                if (res.status !== 1) {
                     return layer.msg('登录失败')
                 }
                 layer.msg('登录成功')
